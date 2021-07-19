ad73332b5131f0cde939828ee141280fc525e23b
Rodrigo <rodrigo.n.marques1@gmail.com>
2021-07-19 22:31:59 +0100

    navbar + sticky mesmo quase completo

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ document.querySelector('#moon').addEventListener('click' , () => {
 	document.body.classList.toggle('dark')
 })
 
+const navbar = document.querySelector('.navbar')
+const sticky = navbar.offsetTop
+
+function stickyNavbar() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add('sticky')
+	} else {
+		navbar.classList.remove('sticky');
+	}
+}
+
+window.onscroll = function() {stickyNavbar()};
