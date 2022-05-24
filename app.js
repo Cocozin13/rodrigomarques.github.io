8adc96a72ee06572203fdbd87f926df40993b444
Cocozin13 <atumbananaxd2@gmail.com>
2022-05-25 00:35:44 +0100

    test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Sticky Navbar
 document.querySelector('#moon').addEventListener('click' , () => {
 	document.body.classList.toggle('dark')
 })
@@ -12,5 +13,60 @@ function stickyNavbar() {
 		navbar.classList.remove('sticky');
 	}
 }
-
 window.onscroll = function() {stickyNavbar()};
+
+//Typewritter Effect
+
+const textDis = document.getElementById("devText");
+const phrases = ["Front End Dev","Web Developer"];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop ()
+{
+	isEnd = false
+	textDis.textContent = currentPhrase.join("");
+
+	if (i < phrases.length)
+	{
+		if (!isDeleting && j <= phrases[i].length)
+		{
+			currentPhrase.push(phrases[i][j]);
+			j++
+			textDis.textContent = currentPhrase.join("");
+		}
+
+		if (isDeleting && j <= phrases[i].length)
+		{
+			currentPhrase.pop(phrases[i][j]);
+			j--
+			textDis.textContent = currentPhrase.join("");
+		}
+
+		if (j == phrases[i].length)
+		{
+			isEnd = true;
+			isDeleting = true;
+		}
+
+		if (isDeleting && j === 0)
+		{
+			currentPhrase = [];
+			isDeleting = false;
+			i++
+			if (i === phrases.length)
+			{
+				i = 0;
+			}
+		}
+	}
+	const spedUp = Math.random() * (80 -50) + 50;
+	const normalSpeed = Math.random() * (300 -200) + 200;
+	const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+	setTimeout(loop, time);
+}
+
+loop();
---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "rodrigo.n.marques1@gmail.com";
+    $headers = "E-Mail recebido: ".$mailFrom;
+    $txt = "Mensagem de: ".$name."\n\n.$message";
+
+    mail($mailTo, $subject, $txt; $headers);
+    header("Localização: index.html?mailsend");
+}
